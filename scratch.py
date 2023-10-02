1b1635adb5b5f9985517825c08197685c9a19b75
randall-pulido <rp86254@gmail.com>
2023-10-02 14:32:17 -0700

    resolve merge issues

---
+++ scratch.py
@@ -0,0 +1,6 @@
+from helpers import clean_lyrics
+
+text = '\nWhat did she say to meyou might also like as I emptiedsee george strait live get tickets as low as $73my pockets.'
+
+print(text)
+print(clean_lyrics(text))
